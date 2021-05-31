02a162d8adfc977536b0138a8579b298b8baeed1
Rafael Cunha <rafaelnogcunha@hotmal.com>
2021-05-31 20:47:40 -0300

    Updates

---
+++ precache-manifest.02331e8ba0a5973704289f9599bdd2f0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a6113b613a12f15ea0919f0036bd02a",
+    "url": "/portfael/index.html"
+  },
+  {
+    "revision": "c74c8ac3373e05ff281e",
+    "url": "/portfael/static/css/2.709b0bf7.chunk.css"
+  },
+  {
+    "revision": "f791a08d691f9f92dc48",
+    "url": "/portfael/static/css/main.ac794018.chunk.css"
+  },
+  {
+    "revision": "c74c8ac3373e05ff281e",
+    "url": "/portfael/static/js/2.0e3060da.chunk.js"
+  },
+  {
+    "revision": "f791a08d691f9f92dc48",
+    "url": "/portfael/static/js/main.395988dc.chunk.js"
+  },
+  {
+    "revision": "757484b014804cbfd339",
+    "url": "/portfael/static/js/runtime-main.21f2d5ff.js"
+  },
+  {
+    "revision": "0de06a760cbac80361afbde9205bdca3",
+    "url": "/portfael/static/media/bulba.0de06a76.svg"
+  },
+  {
+    "revision": "96f7793d6a70ad3d7ce50e0825959ca7",
+    "url": "/portfael/static/media/dev.96f7793d.svg"
+  },
+  {
+    "revision": "c6d006c70c112913ecca8b05fa3fc422",
+    "url": "/portfael/static/media/docs.c6d006c7.svg"
+  },
+  {
+    "revision": "3d423714dc7fd592a93f98351a7100a5",
+    "url": "/portfael/static/media/ghub.3d423714.svg"
+  },
+  {
+    "revision": "ce9bf85195c591b1dd182c11a2ebd59c",
+    "url": "/portfael/static/media/glab.ce9bf851.svg"
+  },
+  {
+    "revision": "3aee7e5cb6cfe91631a144cf09ab9cd9",
+    "url": "/portfael/static/media/lnkdin.3aee7e5c.svg"
+  },
+  {
+    "revision": "bebd3765f55592d09ed10a4b5ce1f7fa",
+    "url": "/portfael/static/media/perfil1.bebd3765.jpg"
+  },
+  {
+    "revision": "a624872f623a2b81064c7a21fbb58a6c",
+    "url": "/portfael/static/media/youtube.a624872f.svg"
+  }
+]);
